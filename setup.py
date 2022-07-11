da479184f755c7792ccddc45ce8cb7b320e9a1b7
Rohit Bharti <rohit.bharti8211@gmail.com>
2022-07-11 23:45:06 +0530

    new files updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Thyroid-detection",
+    version="0.0.3",
+    description="ML project",
+    author="Rohit Bharti",
+    packages=find_packages(),
+    license="Apache 2.0"
+)
